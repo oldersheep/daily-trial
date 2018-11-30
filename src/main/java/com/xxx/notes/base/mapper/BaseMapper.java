f980ffee5b7a6b45c768ce18ef7a6757f1c367bd
liliangan <liliangan@h3c.com>
2018-11-30 10:51:49 +0800

    Revert "通用mapper中添加批量删除，不是很通用感觉"
    This reverts commit 0745c0f

--- src/main/java/com/xxx/notes/base/mapper/BaseMapper.java
+++ src/main/java/com/xxx/notes/base/mapper/BaseMapper.java
@@ -1,8 +1,5 @@
 package com.xxx.notes.base.mapper;
 
-import org.apache.ibatis.annotations.DeleteProvider;
-import org.apache.ibatis.annotations.Param;
-import tk.mybatis.mapper.annotation.RegisterMapper;
 import tk.mybatis.mapper.common.Mapper;
 import tk.mybatis.mapper.common.MySqlMapper;
 
@@ -13,15 +10,6 @@ import tk.mybatis.mapper.common.MySqlMapper;
  * @Date 2018/7/20 17:36
  * @Version V1.0
  */
-@RegisterMapper
 public interface BaseMapper<T> extends Mapper<T>, MySqlMapper<T> {
 
-    /**
-     * 根据主键ID批量删除
-     *
-     * @param key
-     * @return
-     */
-    @DeleteProvider(type = BaseMapperProvider.class, method = "dynamicSQL")
-    int deleteByIDS(Object[] key);
 }
